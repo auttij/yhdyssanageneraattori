4efcf3b1c75b0866e94d3f53a199ae1880f43378
auttij <juhana.autti@gmail.com>
2023-03-01 20:59:45 +0200

    Improve performance

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 from string import Template
-from itertools import product
+from itertools import product, takewhile
 from time import perf_counter
 
 MINIMUM_WORD_LENGTH = 3
@@ -53,45 +53,65 @@ def make_combinations(base, a, b):
     return (f"{i}{j[l:]}" for (i, j) in product(b, a))
 
 def main(source, output):
+    # initializing, reading file, sorting.
     wordlist = read_file(source)
+    string_sorted_wordlist = sorted(wordlist)
     length_sorted_wordlist = sorted(wordlist, key=lambda x: len(x))
     breakpoints = find_breakpoints(length_sorted_wordlist)
+
+    # Helper for checking if the remainder of a word with a part taken out is a word.
+    # Used for trying to figure out if the word was a compound word.
+    def is_word(word):
+        if len(word) < 3:
+            return False
+
+        search_result = binary_search(string_sorted_wordlist, string_comparison_func(word))
+        match = string_sorted_wordlist[search_result]
+        return match == word
 
     total_word_count = len(wordlist)
     for l in range(MINIMUM_WORD_LENGTH, MAXIMUM_WORD_LENGTH):
         start_time = perf_counter()
 
-        print(f"looking for words with length {l}, like {length_sorted_wordlist[breakpoints[l]]}")
-
-        range_size = breakpoints[l + 1] - breakpoints[l]
-        size_to_end = total_word_count - ((breakpoints[l + 1] + breakpoints[l]) // 2)
+        # calculations for counting progress
+        range_start, range_end = breakpoints[l], breakpoints[l + 1]
+        range_size = range_end - range_start
+        size_to_end = total_word_count - ((range_end + range_start) // 2)
         range_total = (range_size * size_to_end) + range_size // 2
         count = 0
 
-        for i in range(breakpoints[l], breakpoints[l + 1]):
+        print(f"looking for words with length {l}, {range_size} in total")
+
+        # iterate all words of length l
+        for i in range(range_start, range_end):
             print(f'progress {count} / {range_total}', end='\r')
             base = length_sorted_wordlist[i]
 
-            def isWord(word):
-                if len(word) < 3:
-                    return False
+            base_index = string_sorted_wordlist.index(base)
+            matching_start = [
+                word for word 
+                in takewhile(
+                    lambda x: x[:l] == base, 
+                    string_sorted_wordlist[base_index:]
+                ) 
+                if is_word(word[l:])
+            ]
 
-                search_result = binary_search(wordlist, string_comparison_func(word))
-                match = wordlist[search_result]
-                return match == word
+            matching_end = [
+                word for word 
+                in length_sorted_wordlist[i+1:] 
+                if word[-l:] == base 
+                    and is_word(word[:-l])
+            ]
 
-            matching_start = [word for word in length_sorted_wordlist[i+1:] if word[:l] == base and isWord(word[l:])]
-
-            matching_end = [word for word in length_sorted_wordlist[i+1:] if word[-l:] == base and isWord(word[:-l])]
-
-            if len(matching_start) and len(matching_end):
+            if matching_start and matching_end:
                 combinations = make_combinations(base, matching_start, matching_end)
                 filename = output.safe_substitute(file_num=f'{l:02d}')
                 append_to_file(filename, combinations)
             count += (total_word_count - i)
 
         end_time = perf_counter()
-        print(f"{count} words checked. time elapsed: {end_time - start_time:0.1f} seconds")
+        print(f"{count} combinations checked. time elapsed: {end_time - start_time:0.1f} seconds")
 
 if __name__ == '__main__':
     main(SOURCE, OUTPUT)
